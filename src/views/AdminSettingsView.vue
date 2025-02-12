bcc501f1c9cb92baf4d422397069766c7ef697dc
vik <fake@email.com>
2025-02-12 16:04:44 +0500

    added upload from frontend

--- src/views/AdminSettingsView.vue
+++ src/views/AdminSettingsView.vue
@@ -5,48 +5,58 @@
         <v-tab value="general">Основные</v-tab>
         <v-tab value="data-upload">Загрузка данных</v-tab>
       </v-tabs>
-
       <v-tabs-items v-model="activeTab">
         <v-tab-item value="general">
           <!-- Содержимое вкладки "Основные" -->
         </v-tab-item>
-
         <v-tab-item value="data-upload">
           <v-card-text>
             <v-form ref="form">
+              <!-- Загрузка первого файла -->
               <v-file-input
-                label="Загрузить файл 1"
-                accept=".csv"
-                prepend-inner-icon="mdi-upload"
-                @change="handleFileUpload(1, $event)"
+                  label="Загрузить файл (.xlsx)"
+                  accept=".xlsx"
+                  prepend-inner-icon="mdi-upload"
+                  @change="handleFileUpload($event)"
+                  :disabled="isLoading"
               ></v-file-input>
 
-              <v-file-input
-                label="Загрузить файл 2"
-                accept=".csv"
-                prepend-inner-icon="mdi-upload"
-                @change="handleFileUpload(2, $event)"
-              ></v-file-input>
+              <!-- Пустышка для второго файла -->
+              <v-text-field
+                  label="Информация о втором файле"
+                  readonly
+                  hint="Этот файл не требуется."
+                  persistent-hint
+              ></v-text-field>
 
-              <v-btn color="primary" @click="uploadFiles" class="mt-4"
-                >Загрузить</v-btn
+              <!-- Кнопка загрузки -->
+              <v-btn
+                  color="primary"
+                  @click="uploadFile"
+                  class="mt-4"
+                  :loading="isLoading"
+                  :disabled="!selectedFile"
               >
-            </v-form>
+                Загрузить
+              </v-btn>
 
-            <div v-if="uploadedFiles.length">
-              <v-divider class="my-4"></v-divider>
-              <v-list>
-                <v-list-item
-                  v-for="(file, index) in uploadedFiles"
-                  :key="index"
-                >
-                  <v-list-item-avatar>
-                    <v-icon color="primary">mdi-file</v-icon>
-                  </v-list-item-avatar>
-                  <v-list-item-title>{{ file.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </div>
+              <!-- Анимация загрузки и статус -->
+              <div v-if="isLoading" class="mt-4">
+                <v-progress-circular
+                    indeterminate
+                    color="primary"
+                    size="24"
+                    width="2"
+                ></v-progress-circular>
+                <span class="ml-2">Загрузка...</span>
+              </div>
+
+              <!-- Успешная загрузка -->
+              <div v-if="isSuccess" class="mt-4">
+                <v-icon color="success">mdi-check-circle</v-icon>
+                <span class="ml-2">Файл успешно загружен!</span>
+              </div>
+            </v-form>
           </v-card-text>
         </v-tab-item>
       </v-tabs-items>
@@ -55,39 +65,59 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'AdminSettingsView',
   data() {
     return {
       activeTab: 'data-upload',
-      uploadedFiles: [],
-      selectedFiles: {},
+      selectedFile: null,
+      isLoading: false,
+      isSuccess: false,
     };
   },
   methods: {
-    handleFileUpload(fileIndex, event) {
-      const file = event;
-      if (file) {
-        this.$set(this.selectedFiles, fileIndex, file);
-      }
+    handleFileUpload(event) {
+      this.selectedFile = event.target.files[0];
+      this.isSuccess = false; // Сбрасываем статус успеха при выборе нового файла
     },
-    uploadFiles() {
-      for (let key in this.selectedFiles) {
-        const file = this.selectedFiles[key];
-        if (file && file.type === 'text/csv') {
-          this.uploadedFiles.push(file);
-          this.$emit('file-uploaded', { fileIndex: key, file });
+    async uploadFile() {
+      if (!this.selectedFile) {
+        alert('Пожалуйста, выберите файл.');
+        return;
+      }
+      this.isLoading = true;
+      try {
+        const formData = new FormData();
+        formData.append('file', this.selectedFile);
+        console.log(this.selectedFile);
+        console.log(formData)
+        const response = await axios.post(
+            `${process.env.VUE_APP_API_URL}/upload/student-choices`,
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+              },
+            }
+        );
+
+        if (response.status === 200) {
+          this.isSuccess = true;
         } else {
-          this.$refs.form.resetValidation();
-          alert('Пожалуйста, загрузите только CSV файлы.');
+          alert('Ошибка при загрузке файла.');
         }
+      } catch (error) {
+        console.error('Ошибка при загрузке файла:', error);
+        alert('Произошла ошибка при загрузке файла. Попробуйте еще раз.');
+      } finally {
+        this.isLoading = false;
       }
-      this.selectedFiles = {};
-    },
+    }
   },
 };
 </script>
 
 <style scoped>
-
 </style>
