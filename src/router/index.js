f264864809bc40e1e17e4158ddc0b60a1a3df42d
vikku <vita.201581@yandex.ru>
2025-02-24 01:44:11 +0500

    added route for all transfers

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import {createRouter, createWebHistory} from 'vue-router';
 import LoginView from '../views/LoginView.vue';
 import UserView from '../views/UserView.vue';
 import AdminView from '../views/AdminView.vue';
+import AdminRequestsView from '../views/AdminRequestsView.vue';
 import AdminSettingsView from '../views/AdminSettingsView.vue';
 import UserProfileView from '../views/UserProfileView.vue';
 import CourseDetail from '../views/CourseDetail.vue';
 import RequestView from '../views/RequestView.vue';
-import NotFoundView from '../views/NotFoundView.vue'
-
+import NotFoundView from '../views/NotFoundView.vue';
 
 const routes = [
     {
@@ -38,18 +38,16 @@ const routes = [
         meta: {requiresAuth: true, role: 'admin'},
     },
     {
+        path: '/admin/requests',
+        name: 'AdminRequestsView',
+        component: AdminRequestsView,
+        meta: {requiresAuth: true, role: 'admin'},
+    },
+    {
         path: '/profile',
         name: 'UserProfileView',
         component: UserProfileView,
         meta: {requiresAuth: true, role: 'user'},
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        name: 'NotFound',
-        component: NotFoundView,
-        meta: {
-            requiresAuth: false
-        }
     },
     {
         path: '/courses/:id',
@@ -63,6 +61,14 @@ const routes = [
         name: 'RequestView',
         component: RequestView,
         meta: {requiresAuth: true, role: 'user'}
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFoundView,
+        meta: {
+            requiresAuth: false
+        }
     }
 ];
 
